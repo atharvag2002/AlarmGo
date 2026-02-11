41cb853736869c17f3b31e8ac7d37265b1e136ef
Atharva Gaikwad <atharvagaikwad125@gmail.com>
2026-02-11 21:12:26 +0530

    user and destination tracking + distance calculation

--- app/screens/map.tsx
+++ app/screens/map.tsx
@@ -1,10 +1,31 @@
 import * as Location from "expo-location";
+import * as Notifications from 'expo-notifications';
+import { getDistance } from 'geolib';
 import { useEffect, useState } from "react";
-import { StyleSheet, TextInput, View } from "react-native";
-import MapView, { Region } from "react-native-maps";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import MapView, { LatLng, Marker, Region } from "react-native-maps";
 
 export default function Map() {
-  const [region, setRegion] = useState<Region | null>(null);
+  const [userlocation, setUserlocation] = useState<Region | null>(null);
+  const [destination , setDestination] = useState<LatLng | null>(null);
+  let distance = 0;
+  
+  if(userlocation&&destination){
+     distance = getDistance(
+    {latitude: userlocation?.latitude, longitude: userlocation?.longitude},
+    {latitude: destination?.latitude, longitude: destination?.longitude})
+  }
+
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldPlaySound: false,
+      shouldSetBadge:false,
+      shouldShowBanner:false,
+      shouldShowList:false 
+    }),
+  });
+  
+
 
   useEffect(() => {
     (async () => {
@@ -17,7 +38,7 @@ export default function Map() {
       console.log(status);
 
 
-      setRegion({
+      setUserlocation({
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
         latitudeDelta: 0.01,
@@ -28,12 +49,20 @@ export default function Map() {
 
   return (
     <View style={styles.container}>
-        <TextInput style={styles.textinput} />
+        <TextInput style={styles.textinput} placeholder="Hii" />
       <MapView
         style={styles.map}
         showsUserLocation
-        region={region ?? undefined}
-      />
+        region={userlocation ?? undefined}
+        onPress={(e) => setDestination(e.nativeEvent.coordinate)}
+        >   
+        {destination && <Marker coordinate={destination} />}
+        </MapView>
+        <Text style={styles.text}> 
+          USER :{userlocation?.longitude}| {userlocation?.latitude}  {`\n`} 
+          DESTINATION :{destination?.longitude}| {destination?.latitude}   {`\n`} {`\n`}
+          DISTANCE : {distance}
+        </Text>
     </View>
   );
 }
@@ -48,5 +77,23 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     backgroundColor: '#fff',
     fontSize: 16,
-    marginBottom: 20, }
+    marginBottom: 20, 
+    position:"absolute",
+    top:20,
+    left:15,
+    right:15,
+    zIndex: 10,      // important
+    elevation: 10, 
+  },
+  text:{
+    backgroundColor:'black',
+    position:"absolute",
+    bottom:30,
+    left:15,
+    right:15,
+    zIndex: 10,      // important
+    elevation: 10,
+    color:'white'
+    
+  }
 });
